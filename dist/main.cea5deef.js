7b72b3e007b9b25ab2b0816cbd7e9a331be90f44
Wagner Araújo <wagneraraujo15@gmail.com>
2020-08-27 22:00:24 -0300

    update iten

--- dist/main.cea5deef.js
+++ dist/main.cea5deef.js
@@ -231,7 +231,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36933" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38071" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
