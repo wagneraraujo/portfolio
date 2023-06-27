54945e47951fc65b59000e6bf2d7229a3d339b4d
Wagner Araújo <wagneraraujo15@gmail.com>
2023-06-27 18:46:44 -0300

    insert apps in portfólio

---
+++ dist/index.074273a0.js
@@ -0,0 +1,2 @@
+!function(){let e=document.querySelector("#nav-icon1"),o=document.querySelector(".mobileMenu");e.addEventListener("click",function(){e.classList.toggle("open"),o.classList.toggle("menuMobileOpen"),console.log("clicass")})}(),document.querySelector(".mobileMenu");
+//# sourceMappingURL=index.074273a0.js.map
