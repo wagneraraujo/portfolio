1338be2a99537a1c3a61bb2af93bfee28bf534aa
Wagner Araújo <wagneraraujo15@gmail.com>
2024-06-24 08:37:23 -0300

    build  updates portfolio

---
+++ dist/index.79f3a1d2.js
@@ -0,0 +1,2 @@
+let e,o;e=document.querySelector("#nav-icon1"),o=document.querySelector(".mobileMenu"),e.addEventListener("click",function(){e.classList.toggle("open"),o.classList.toggle("menuMobileOpen"),console.log("clicass")}),document.querySelector(".mobileMenu");
+//# sourceMappingURL=index.79f3a1d2.js.map
